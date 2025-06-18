62b896264d5947db9d0feb6d604be678a2c8ee34
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-06-18 13:23:35 -0300

    JSX

--- src/Components/ListaPersonagem.jsx
+++ src/Components/ListaPersonagem.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import md5 from 'md5';
-import './ListaPersonagem.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import md5 from "md5";
+import "./ListaPersonagem.css";
 
-const PUBLIC_KEY = '3e8bef26ce91bf500ab574be3cc76285';
-const PRIVATE_KEY = '450a95fff14e78a0aa9686b4d4889bfd52e90435';
+const PUBLIC_KEY = "3e8bef26ce91bf500ab574be3cc76285";
+const PRIVATE_KEY = "450a95fff14e78a0aa9686b4d4889bfd52e90435";
 
 const MarvelCharacters = () => {
   const [characters, setCharacters] = useState([]);
@@ -20,7 +20,7 @@ const MarvelCharacters = () => {
         const response = await axios.get(url);
         setCharacters(response.data.data.results);
       } catch (error) {
-        console.error('Erro ao buscar personagens:', error);
+        console.error("Erro ao buscar personagens:", error);
       } finally {
         setLoading(false);
       }
@@ -32,29 +32,25 @@ const MarvelCharacters = () => {
   if (loading) return <p>Carregando personagens...</p>;
 
   return (
-<<<<<<< HEAD
-    <div>
-      <h1> PERSONAGENS DA MARVEL </h1>
-      <ul>
-=======
-    <div className="container">
-      <h1 className="titulo">Personagens da Marvel</h1>
-      <div className="grid">
->>>>>>> 6ab07e2f92f6beb6ea087702dd9efc0da84d0ae7
-        {characters.map((char) => (
-          <div key={char.id} className="card">
-            <h2 className="nome">{char.name}</h2>
-            <div className="imagem">
-              <img
-                src={`${char.thumbnail.path}.${char.thumbnail.extension}`}
-                alt={char.name}
-              />
+    <>
+          <div className="container">
+        <h1 className="titulo">PERSONAGENS DA MARVEL </h1>
+        <div className="grid">
+          {characters.map((char) => (
+            <div key={char.id} className="card">
+              <h2 className="nome">{char.name}</h2>
+              <div className="imagem">
+                <img
+                  src={`${char.thumbnail.path}.${char.thumbnail.extension}`}
+                  alt={char.name}
+                />
+              </div>
+              <button className="botao">Favoritar</button>
             </div>
-            <button className="botao">Favoritar</button>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
