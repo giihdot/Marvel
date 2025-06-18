aa95836534ed805d65bce39de35ea8a3021b8d4d
Kamilly Barra <kamillye033@gmail.com>
2025-06-18 10:34:57 -0300

    Alteração texto e imagens

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -11,22 +11,32 @@ export default function Home() {
       <h1 style={{ fontSize: "3rem", color: "#e62429" }}>
         Bem-vindo ao MarvelVerse! 🦸‍♂️
       </h1>
-      <p style={{ fontSize: "1.2rem" }}>
+
+      <p style={{ fontSize: "1.2rem", maxWidth: "700px", margin: "0 auto" }}>
         Prepare-se para embarcar em uma jornada épica onde heróis enfrentam
         vilões, poderes desafiam a lógica e cada página conta uma nova aventura!
-        Aqui, você encontra: ✨ Super-heróis lendários como Homem de Ferro,
-        Capitão América, Thor e a poderosa Capitã Marvel. 🔍 Busque pelo seu
-        personagem favorito, conheça sua história, suas batalhas e suas
-        curiosidades. 🧠 Descubra detalhes escondidos, quadrinhos clássicos e
-        séries incríveis direto do multiverso Marvel. Este não é apenas um
-        app... É seu portal para o MarvelVerse, onde você é o herói da própria
-        experiência! "Com grandes poderes, vêm grandes responsabilidades." - Tio
-        Ben E com esse app... vem também muito conteúdo incrível! 🚀{" "}
+        <br /><br />
+        ✨ Super-heróis lendários como Homem de Ferro, Capitão América, Thor e a poderosa Capitã Marvel.
+        <br /><br />
+        🔍 Busque pelo seu personagem favorito, conheça sua história, suas batalhas e suas curiosidades.
+        <br /><br />
+        🧠 Descubra detalhes escondidos, quadrinhos clássicos e séries incríveis direto do multiverso Marvel.
+        <br /><br />
+        Este não é apenas um app... É seu portal para o MarvelVerse, onde você é o herói da própria
+        experiência! <br /><br />
+        <em>"Com grandes poderes, vêm grandes responsabilidades." – Tio Ben</em> <br />
+        E com esse app... vem também muito conteúdo incrível! 🚀
       </p>
+
       <img
-        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.m.wikipedia.org%2Fwiki%2FFicheiro%3AMarvelLogo.svg&psig=AOvVaw3F-sRFTQ-6Ck5T22yglxF2&ust=1750339187849000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiywt-H-40DFQAAAAAdAAAAABAM"
+        src="https://i.annihil.us/u/prod/marvel/i/mg/9/c0/527bb7b37ff55.jpg"
         alt="Marvel Logo"
-        style={{ marginTop: "30px", maxWidth: "300px" }}
+        style={{ marginTop: "30px", maxWidth: "300px", borderRadius: "12px" }}
+      />
+       <img
+        src="https://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg"
+        alt="Marvel"
+        style={{ marginTop: "30px", maxWidth: "300px", borderRadius: "12px" }}
       />
     </div>
   );
