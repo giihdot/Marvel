5f87f0fbed577c257362fdba5d3788a9dbfb96fe
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-06-18 12:02:16 -0300

    App - JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 // import Favoritos from "./Pages/Favoritos"
+import Home from "./Pages/Home";
 import BuscaHerois from "./Components/BuscaHerois";
 import Detalhes from "./Components/DetalhesHeroi";
 import ListaDePersonagens from "./Components/ListaPersonagem";
@@ -19,12 +20,12 @@ function App() {
         <Link to="/buscaHerois"> BUSCA HEROIS </Link>
         <Link to="/listadepersonagens"> LISTA DE PERSONAGENS </Link>
         <Link to="/portal">PORTAL DIMENSIONAL</Link>
-        <Link path="/portal" element={<PortalDimensional />} />
     
       </nav>
 
       <Routes>
         {/* <Route path="/favoritos" element={<Favoritos/>} /> */}
+        <Route path="/" element={<Home/> } />
         <Route path="/buscaHerois" element={<BuscaHerois />} />
         <Route path="/detalhes" element={<Detalhes />} />
         <Route path="/listadepersonagens" element={<ListaDePersonagens />} />
